6b97e097105f6822565d84e1aaf2519ec509f2a9
felipe <felipegs31@gmail.com>
2018-04-07 20:50:55 -0300

    criar eventos

--- src/pages/create-event/create-event.ts
+++ src/pages/create-event/create-event.ts
@@ -15,6 +15,8 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 })
 export class CreateEventPage {
 
+  textData;
+  months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
   selectedDate = {year: 0, date: 0, month: 0}
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
@@ -22,14 +24,27 @@ export class CreateEventPage {
     this.selectedDate.year = this.navParams.get('year');
     this.selectedDate.month = this.navParams.get('month');
     this.selectedDate.date = this.navParams.get('date');
+    this.dateToText();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CreateEventPage');
   }
 
+  dateToText() {
+    this.textData = `${this.selectedDate.date} de ${this.months[this.selectedDate.month]} de ${this.selectedDate.year} `
+  }
+
   onLeave() {
     this.viewCtrl.dismiss();
+  }
+
+  onSave() {
+    console.log();
+  }
+
+  goToMaps() {
+
   }
 
 }
